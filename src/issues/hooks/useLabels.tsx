c69eea33d2335cddefc13fd1ef4cfe3395367b39
Wal Monfortt <71857487+WallMonfortt@users.noreply.github.com>
2022-11-30 17:23:39 -0600

    initial data and placeholder data

--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -7,7 +7,7 @@ import { sleep } from '../../helpers/sleep';
 const getLabels = async():Promise<Label[]> => {
     await sleep(2);
     const {data} = await githubApi.get<Label[]>('/labels');
-    // const response = await fetch('https://api.github.com/repos/facebook/react/labels')
+    // const response = await fetch('https://api.github.com/repos/facebook/react/labels') // this is the same as the line above
     // const data = await response.json()
     console.log(data)
     return data
@@ -19,6 +19,25 @@ export const useLabels = () => {
         getLabels,  // fetcher function
         {
             staleTime: 1000 * 60 * 60, // time data is considered fresh
+            // initialData: [], // initial data to be used is similar to the placeholder data with the main difference that it will be used only the first time the query is run
+            placeholderData: [
+              {
+                id: 725156255,
+                node_id: "MDU6TGFiZWw3MjUxNTYyNTU=",
+                url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue%20(taken)",
+                name: "good first issue (taken)",
+                color: "b60205",
+                default: false,
+              },
+              {
+                id: 1249821345,
+                node_id: "MDU6TGFiZWwxMjQ5ODIxMzQ1",
+                url: "https://api.github.com/repos/facebook/react/labels/Component:%20ESLint%20Rules",
+                name: "Component: ESLint Rules",
+                color: "f7afdb",
+                default: false,
+              }
+            ], // data to show while the query is loading
         }
       );
 

19e89973985c580f60c06ab94c2fa429cddfaee1
Wal Monfortt <71857487+WallMonfortt@users.noreply.github.com>
2022-11-30 15:52:29 -0600

    axios "get"

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,5 +1,21 @@
+import { useQuery } from "@tanstack/react-query"
+import { githubApi } from "../../api/githubApi"
+import { Label } from '../interfaces/label';
+
+const getLabels = async():Promise<Label[]> => {
+  const {data} = await githubApi.get<Label[]>('/labels');
+  // const response = await fetch('https://api.github.com/repos/facebook/react/labels')
+  // const data = await response.json()
+  console.log(data)
+  return data
+}
 
 export const LabelPicker = () => {
+
+  const labelsQuery = useQuery(
+    ['labels'],
+    getLabels,
+  );
   return (
     <div>
         <span 

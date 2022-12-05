761e2164c873a85479117b45da8ede276ab73323
Wal Monfortt <71857487+WallMonfortt@users.noreply.github.com>
2022-12-05 17:29:11 -0600

    load comments from issue

--- src/issues/hooks/useIssue.tsx
+++ src/issues/hooks/useIssue.tsx
@@ -3,24 +3,37 @@ import { githubApi } from '../../api/githubApi';
 import { Issue } from '../interfaces';
 import { sleep } from '../../helpers/sleep';
 
-const getIssueInfo = async (issueNumber: number): Promise<Issue> => {
-    await sleep(3)
-    const { data } = await githubApi.get<Issue>(`/issues/${issueNumber}`);
-    console.log(data);
+const getIssueInfo = async( issueNumber: number ):Promise<Issue> => {
+    await sleep(2);
+    const { data } = await githubApi.get<Issue>(`/issues/${ issueNumber }`);
+    return data;
+}
+
+const getIssueComments = async( issueNumber: number ):Promise<Issue[]> => {
+    await sleep(2);
+    const { data } = await githubApi.get<Issue[]>(`/issues/${ issueNumber }/comments`);
     return data;
 }
 
 
-export const useIssue = ( issueNumber : number ) => {
-
+export const useIssue = ( issueNumber: number ) => {
+    
     const issueQuery = useQuery(
-        ['issue', issueNumber], // be careful with the key, it must be unique and the type must be the same as the one used in the query
-        () => getIssueInfo(issueNumber),
+        ['issue', issueNumber ],
+        () => getIssueInfo( issueNumber ),
     );
 
-  return {
-        issueQuery
-    }
-  
-}
+    const issueCommentsQuery = useQuery(
+        ['issue', issueNumber, 'comments' ],
+        () => getIssueComments( issueQuery.data!.number ),
+        {
+            enabled: issueQuery.data !== undefined,
+        }
+    );
 
+
+    return {
+        issueQuery,
+        issueCommentsQuery,
+    }
+}
